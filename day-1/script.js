be00d332ad5ea205f88043199793685f1c998ca8
Neeraj Saini <neeraj.saini@greychaindesign.com>
2023-08-24 20:01:12 +0530

    day-1

---
+++ day-1/script.js
@@ -0,0 +1,54 @@
+"use strict";
+
+// let a = "My name";
+// let a = '45';
+// let a = 45;
+// let a = true;
+// let a = false;
+
+// let var2 = "Nandkishor";
+// var2 = "Neeraj";
+// var2 = "Sumit";
+// var2 = 45;
+
+// const notchange = "Nandkishor";
+
+// var HomeTitle = "Nandkishor";
+
+// console.log(HomeTitle);
+
+// HomeTitle = 45;
+
+// let b = 343434;
+// console.log(HomeTitle);
+
+// console.log(typeof a);
+
+function stringLength() {
+    console.log(x * y);
+    console.log(x + y);
+    console.log(x - y);
+    console.log(x / y);
+
+    console.log("Neeraj");
+}
+
+
+var x = 50;
+
+var y = 20;
+
+
+stringLength();
+
+
+
+
+
+
+
+
+
+
+
+
