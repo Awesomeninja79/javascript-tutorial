ff2d8e6662b3bad1f18d0e1bd56bd3d85c6a5f50
Neeraj Saini <nsrath@Neeraj>
2023-08-29 21:57:34 +0530

    userList api php

---
+++ api/v2/userList.php
@@ -0,0 +1,17 @@
+<?php
+$client_id = 'user';
+$client_secret = 7303448803;
+header('Authorization: Basic '.base64_encode("$client_id:$client_secret"));
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Origin: https://themejunction.in/');
+header('Content-Type: application/json; charset=utf-8');
+
+$data = array(
+    array("user_id" => 1, "username" => "Nandkishor", "user_email" => "nandkishor@gmail.com", "user_status" => true),
+    array("user_id" => 2, "username" => "Neeraj", "user_email" => "neeraj@gmail.com", "user_status" => false),
+    array("user_id" => 3, "username" => "Bob", "user_email" => "bob@gmail.com", "user_status" => false),
+    array("user_id" => 4, "username" => "John", "user_email" => "john@gmail.com", "user_status" => true),
+    array("user_id" => 5, "username" => "Mark", "user_email" => "mark@gmail.com", "user_status" => true),
+    array("user_id" => 6, "username" => "Ville", "user_email" => "ville@gmail.com", "user_status" => false)
+);
+echo json_encode($data);
