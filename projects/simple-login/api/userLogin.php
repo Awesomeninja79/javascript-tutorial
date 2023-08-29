29b001223818ac01fc341ebd3c7b9a098ba43a7d
Neeraj Saini <nsrath@Neeraj>
2023-08-30 03:25:43 +0530

    projects -> api done

--- projects/simple-login/api/userLogin.php
+++ projects/simple-login/api/userLogin.php
@@ -2,11 +2,11 @@
 require_once "db.php";
 
 if (empty($_GET['useremail'])) {
-    setError(401, "Email cannot be empty!");
+    setError(404, "Email cannot be empty!");
 }
 
 if (empty($_GET['password'])) {
-    setError(401, "Password cannot be empty!");
+    setError(404, "Password cannot be empty!");
 }
 
 $sqlQuery = "SELECT user_id, user_email, user_password, user_gender FROM users WHERE user_email='".$_GET['useremail']."' AND user_password='".$_GET['password']."'";
@@ -14,7 +14,7 @@ $sqlQuery = "SELECT user_id, user_email, user_password, user_gender FROM users W
 $result = $mysqli->query($sqlQuery);
 
 if($result->num_rows == 0) {
-    setError(401, "Please check your credentials!");
+    setError(404, "Please check your credentials!");
 }
 
 $data = $result->fetch_assoc();
